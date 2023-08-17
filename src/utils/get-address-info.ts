92c74a9c338bedf393eb4200db5f89b2ea9fcd3c
CarmichaelF <carmichaelferreira06@gmail.com>
2023-08-16 22:37:12 -0300

    fix: fixing unit tests

--- src/utils/get-address-info.ts
+++ src/utils/get-address-info.ts
@@ -1,5 +1,6 @@
 import { env } from '@/env'
-import {AddressType, Client} from '@googlemaps/google-maps-services-js'
+import { InvalidAddress } from '@/use-cases/errors/org-invalid-address-error'
+import { AddressType, Client } from '@googlemaps/google-maps-services-js'
 
 interface GetAddressInfoParams {
   address: string;
@@ -12,28 +13,36 @@ export async function getAddressInfo({
 }: GetAddressInfoParams) {
 	const client = new Client({})
 
-	const result = await client
-		.geocode({
+	try {
+		const result = await client.geocode({
 			params: {
 				key: env.MAPS_API_KEY,
 				address: `${address}, ${zipcode}`,
 			},
 		})
 
-	const place = await client
-		.placeDetails({
+		const place = await client.placeDetails({
 			params: {
 				key: env.MAPS_API_KEY,
 				place_id: result.data.results[0].place_id,
-			}
+			},
 		})
-		
-	const findAddressComponent = (type: AddressType) => {
-		const component = place.data.result.address_components?.find((component) => component.types.includes(type))
-		return component?.long_name
+
+		const findAddressComponent = (type: AddressType) => {
+			const component = place.data.result.address_components?.find(
+				(component) => component.types.includes(type)
+			)
+			return component?.long_name
+		}
+
+		const city = findAddressComponent(AddressType.administrative_area_level_2)
+
+		return {
+			city,
+			...result.data.results[0].geometry.location,
+			formattedAddress: result.data.results[0].formatted_address,
+		}
+	} catch (error) {
+		throw new InvalidAddress()
 	}
-
-	const city = findAddressComponent(AddressType.administrative_area_level_2)
-
-	return {city, ...result.data.results[0].geometry.location, formattedAddress: result.data.results[0].formatted_address}
 }
